8a71caa46c2dbf6a6fb7a7cb5637190e909726bf
spainn <148598154+spainn@users.noreply.github.com>
2024-03-15 09:38:52 -0500

    Added composable function DigitalTime to display a time and the layout of a composable Alarm function that will provide one instance of an alarm.

--- app/src/main/java/com/example/malarm/MainActivity.kt
+++ app/src/main/java/com/example/malarm/MainActivity.kt
@@ -9,11 +9,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.malarm.ui.theme.MalarmTheme
 import androidx.compose.runtime.*
 import kotlinx.coroutines.delay
 import android.icu.util.Calendar
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,26 +53,51 @@ class MainActivity : ComponentActivity() {
 
             MalarmTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+//                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+//                    Greeting("Android")
+//                }
+
+                Alarm()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Alarm(modifier: Modifier = Modifier) {
+    Surface(shape = MaterialTheme.shapes.large, shadowElevation = 1.dp) {
+        Box(
+            modifier = Modifier
+                .padding(1.dp)
+                .fillMaxSize(),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Row {
+                DigitalTime("01", "37", "AM")
+                Spacer(modifier = Modifier.width(40.dp))
+                DigitalTime("02", "37", "AM")
+            }
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun AlarmPreview() {
     MalarmTheme {
-        Greeting("Android")
+        Alarm()
     }
+}
+
+@Composable
+fun DigitalTime(
+    hour: String,
+    minute: String,
+    amOrPm: String
+) {
+    Text(
+        text = "$hour:$minute $amOrPm",
+        style = MaterialTheme.typography.titleLarge
+    )
 }
